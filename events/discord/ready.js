d4a8fa21c1d83b5e76f575206bac4d16ba797c10
Ken_Cir0909 <65713982+KenCir0909@users.noreply.github.com>
2021-03-20 08:53:07 +0900

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -4,7 +4,7 @@ const { Client } = require('discord.js');
  * @param {Client} client
  */
 
-module.exports = (client) => {
+module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setPresence({ activity: { name: '幻想地底国 東方弾幕Bot' }, status: 'dnd' });
 }
